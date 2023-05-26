9c53503399baa7339f9a18d0fa2934f2ec6d4797
Wita Dewisari Tasya <angelatasya3911@gmail.com>
2023-05-26 07:14:32 +0700

    App Icon, Dummy 3D Assets

--- FaneyeAR/View/ContentView.swift
+++ FaneyeAR/View/ContentView.swift
@@ -91,9 +91,9 @@ struct ARViewContainer: UIViewRepresentable {
         
         // Ini dari kode bawaan
         // Load the "Box" scene from the "Experience" Reality File
-        // let boxAnchor = try! Experience.loadBox()
+         let boxAnchor = try! Experience.loadBox()
         // Add the box anchor to the scene
-        // arView.scene.anchors.append(boxAnchor)
+         arView.scene.anchors.append(boxAnchor)
         
         return arView
         
